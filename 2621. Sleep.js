ea76544ba9ffb74b6f6202bb31b37381e0f30083
anshumalapandit <152486309+anshumalapandit@users.noreply.github.com>
2024-11-23 00:09:28 +0530

    Create 2621. Sleep.js

---
+++ 2621. Sleep.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+    // resolve fxn will call after millis second
+   await new promise((res)=>{
+       setTimeout(()=>{
+           res()
+       },millis)
+   })
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
